a73eeccc3d9c358860c95ef5868a62609f76f126
Aleksandar Angelevski <aangelevski6@gmail.com>
2023-08-12 15:36:00 -0700

    data

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
+const fs = require("fs")
 const express = require("express")
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)) 
+
 
 const app = express()
 
 const port = 3000
 
-app.get("/", (req,res) => {
-    res
-    .status(200)
-    .json({message:"heloo from the server side",
-    app: "Natures"})
 
-
-})
-
-app.post('/', (req,res) => {
-    res.send("you can post this endpopint")
-
-})
 
 app.listen(port, () => {
     console.log(`runing on port ${3000}...`)
