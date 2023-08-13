8ade6225cc8ba1cb5006e920a16ccd06dfce5d9e
Aleksandar Angelevski <aangelevski6@gmail.com>
2023-08-12 17:17:57 -0700

    get-request-done

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-const fs = require("fs")
+
 const express = require("express")
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)) 
-
+const fs = require('fs')
 
 const app = express()
 
+//get tours from json
+//--------------
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))//read data from api
+
+app.get('/api/v1/tours', (req,res) => {
+    res.status(200)
+    .json({
+        status: "success",
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+})
+
+
+// get app
+
+
+
+
 const port = 3000
 
+app.get('/', (req,res) => {
+    res.status(200).send("heloo")
+})
 
 
 app.listen(port, () => {
-    console.log(`runing on port ${3000}...`)
+    console.log(`runing on port ${port}...`)
 })
